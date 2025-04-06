9c7f0126ec3a90055ff70e6bd6e2c8f82444bccd
LevaniSamosnidze <levanisamsonidze9@gmail.com>
2025-04-06 13:51:35 +0400

    commit

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ def send_Email():
         'name': request.form['full_name'],
         'email': request.form['email']
     }
-
     file_path = os.path.join(app.root_path, 'emails.txt')
+    # შექმნილია თუ არ არსებობს
+    if not os.path.exists(file_path):
+        open(file_path, 'w', encoding='utf-8').close()
     with open(file_path, 'a', encoding='utf-8') as f:
         f.write(str(user) + '\n')
-
     return user
 
 if __name__ == "__main__":
